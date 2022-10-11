360d5a4c5ce559aa3553f4958f3ff056ce6e3256
Jose <rhwkd5012@naver.com>
2022-10-11 23:14:34 +0900

    별하늘 적용

--- src/components/persons/PersonHead.vue
+++ src/components/persons/PersonHead.vue
@@ -8,6 +8,7 @@
     <div class="person__face" :style="personStyle">
       <div class="person__eyes--left"></div>
       <div class="person__eyes--right"></div>
+      <div class="person__mouth"></div>
     </div>
   </div>
 </template>
@@ -22,10 +23,9 @@ export default defineComponent({
 </script>
 <style lang="scss">
 .person__head {
+  position: relative;
   width: 300px;
   height: 350px;
-  display: flex;
-  flex-direction: column;
   .person__hair {
     position: relative;
     border-radius: 40px 40px 0px 0px;
@@ -47,6 +47,38 @@ export default defineComponent({
       height: 110px;
       border-radius: 0 0 110px 0;
     }
+  }
+  .person__eyes {
+    &--left {
+      z-index: 999;
+      top: 160px;
+      left: 50px;
+      width: 30px;
+      height: 30px;
+      border-radius: 100%;
+      position: absolute;
+      background-color: black;
+    }
+    &--right {
+      z-index: 999;
+      top: 160px;
+      right: 50px;
+      width: 30px;
+      height: 30px;
+      border-radius: 100%;
+      position: absolute;
+      background-color: black;
+    }
+  }
+  .person__mouth {
+    top: 300px;
+    left: 150px;
+    transform: translate(-50%, -50%);
+    width: 100px;
+    height: 60px;
+    border-radius: 0 0 100px 100px;
+    position: absolute;
+    background-color: red;
   }
   .person__face {
     z-index: -20;
--- src/views/layouts/MainView.vue
+++ src/views/layouts/MainView.vue
@@ -1,19 +1,24 @@
 <template>
-  <main class="sticky-container" ref="sticky-container">
-    <div class="sticky">
-      <div class="slide-container">
-        <div class="slide" ref="sl1">
-          <HomeView></HomeView>
+  <div class="noite">
+    <main class="sticky-container" ref="sticky-container">
+      <div class="sticky">
+        <div class="slide-container">
+          <div class="constelacao"></div>
+          <div class="chuvaMeteoro"></div>
+          <div class="slide" ref="sl1">
+            <HomeView></HomeView>
+          </div>
+          <div class="slide" ref="sl2">
+            <PersonView></PersonView>
+          </div>
+          <div class="slide" ref="sl3"></div>
+          <div class="slide" ref="sl4"></div>
+          <div class="slide" ref="sl5"></div>
         </div>
-        <div class="slide" ref="sl2">
-          <PersonView></PersonView>
-        </div>
-        <div class="slide" ref="sl3"></div>
-        <div class="slide" ref="sl4"></div>
-        <div class="slide" ref="sl5"></div>
       </div>
-    </div>
-  </main>
+    </main>
+  </div>
+  <div class="constelacao"></div>
 </template>
 <script>
 import { def } from "./index";
@@ -46,6 +51,7 @@ export default {
   },
 
   mounted() {
+    this.skyInit();
     this.init();
     window.addEventListener("scroll", this.onScroll);
   },
@@ -55,6 +61,64 @@ export default {
   },
 
   methods: {
+    skyInit() {
+      //estrelas
+
+      var style = ["style1", "style2", "style3", "style4"];
+      var tam = ["tam1", "tam1", "tam1", "tam2", "tam3"];
+      var opacity = [
+        "opacity1",
+        "opacity1",
+        "opacity1",
+        "opacity2",
+        "opacity2",
+        "opacity3",
+      ];
+
+      function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+
+      var estrela = "";
+      var qtdeEstrelas = 250;
+      var noite = document.querySelector(".constelacao");
+      var widthWindow = window.innerWidth;
+      var heightWindow = window.innerHeight;
+
+      for (var i = 0; i < qtdeEstrelas; i++) {
+        estrela +=
+          "<span class='estrela " +
+          style[getRandomArbitrary(0, 4)] +
+          " " +
+          opacity[getRandomArbitrary(0, 6)] +
+          " " +
+          tam[getRandomArbitrary(0, 5)] +
+          "' style='animation-delay: ." +
+          getRandomArbitrary(0, 9) +
+          "s; left: " +
+          getRandomArbitrary(0, widthWindow) +
+          "px; top: " +
+          getRandomArbitrary(0, heightWindow) +
+          "px;'></span>";
+      }
+
+      noite.innerHTML = estrela;
+      var numeroAleatorio = 5000;
+      setTimeout(function () {
+        carregarMeteoro();
+      }, numeroAleatorio);
+
+      function carregarMeteoro() {
+        setTimeout(carregarMeteoro, numeroAleatorio);
+        numeroAleatorio = getRandomArbitrary(5000, 10000);
+        var meteoro =
+          "<div class='meteoro " + style[getRandomArbitrary(0, 4)] + "'></div>";
+        document.getElementsByClassName("chuvaMeteoro")[0].innerHTML = meteoro;
+        setTimeout(function () {
+          document.getElementsByClassName("chuvaMeteoro")[0].innerHTML = "";
+        }, 1000);
+      }
+    },
     init() {
       this.initAnimation();
     },
@@ -188,6 +252,12 @@ export default {
     justify-content: center;
     width: 100%;
     height: 100%;
+    background: -webkit-linear-gradient(
+      top,
+      rgb(0, 0, 0) 50%,
+      rgb(25, 19, 39) 80%,
+      rgb(43, 32, 72)
+    );
     .slide {
       position: absolute;
       display: none;
@@ -224,5 +294,242 @@ export default {
   height: 50px;
   text-align: center;
   bottom: 0;
+}
+.constelacao {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  animation: rotate 600s infinite linear;
+}
+
+.estrela {
+  background-color: white;
+  border-radius: 50%;
+  position: absolute;
+  animation-name: estrela;
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+}
+
+.estrela.style1 {
+  animation-duration: 0.5s;
+  animation-name: estrela;
+}
+.estrela.style2 {
+  animation-duration: 1s;
+  animation-name: estrela;
+}
+.estrela.style3 {
+  animation-duration: 1.5s;
+  animation-name: estrela;
+}
+.estrela.style4 {
+  animation-duration: 2s;
+  animation-name: estrelaDestacada;
+}
+
+.estrela.tam1 {
+  width: 1px;
+  height: 1px;
+}
+.estrela.tam2 {
+  width: 2px;
+  height: 2px;
+}
+.estrela.tam3 {
+  width: 3px;
+  height: 3px;
+}
+
+.estrela.opacity1 {
+  opacity: 1;
+}
+.estrela.opacity2 {
+  opacity: 0.5;
+}
+.estrela.opacity3 {
+  opacity: 0.1;
+}
+
+.meteoro {
+  position: absolute;
+  background-color: #fff;
+  width: 2px;
+  height: 2px;
+  border-radius: 50%;
+  transform: rotate(-35deg);
+  animation-timing-function: linear;
+  animation-iteration-count: infinite;
+  animation-duration: 1s;
+}
+
+.meteoro:before {
+  content: "";
+  display: inline-block;
+  vertical-align: middle;
+  margin-right: 10px;
+  width: 0;
+  height: 0;
+  border-top: 1px solid transparent;
+  border-bottom: 1px solid transparent;
+  border-left: 85px solid white;
+  position: absolute;
+  left: 2px;
+  top: 0;
+}
+
+.meteoro.style1 {
+  animation-name: meteoroStyle1;
+}
+.meteoro.style2 {
+  animation-name: meteoroStyle2;
+}
+.meteoro.style3 {
+  animation-name: meteoroStyle3;
+}
+.meteoro.style4 {
+  animation-name: meteoroStyle4;
+}
+
+@keyframes escurecer {
+  0% {
+    top: 0;
+  }
+  100% {
+    top: 100%;
+  }
+}
+
+@keyframes estrela {
+  0% {
+    box-shadow: 0 0 10px 0px rgba(255, 255, 255, 0.05);
+  }
+  50% {
+    box-shadow: 0 0 10px 2px rgba(255, 255, 255, 0.4);
+  }
+  100% {
+    box-shadow: 0 0 10px 0px rgba(255, 255, 255, 0.05);
+  }
+}
+
+@keyframes estrelaDestacada {
+  0% {
+    background-color: #ffffff;
+    box-shadow: 0 0 10px 0px rgba(255, 255, 255, 1);
+  }
+  20% {
+    background-color: #ffc4c4;
+    box-shadow: 0 0 10px 0px rgb(255, 196, 196, 1);
+  }
+  80% {
+    background-color: #c4cfff;
+    box-shadow: 0 0 10px 0px rgb(196, 207, 255, 1);
+  }
+  100% {
+    background-color: #ffffff;
+    box-shadow: 0 0 10px 0px rgba(255, 255, 255, 0.2);
+  }
+}
+
+@keyframes meteoroStyle1 {
+  0% {
+    opacity: 0;
+    right: 300px;
+    top: 100px;
+  }
+  30% {
+    opacity: 0.3;
+  }
+  60% {
+    opacity: 0.3;
+  }
+  100% {
+    opacity: 0;
+    right: 1000px;
+    top: 600px;
+  }
+}
+
+@keyframes meteoroStyle2 {
+  0% {
+    opacity: 0;
+    right: 700px;
+    top: 100px;
+  }
+  30% {
+    opacity: 1;
+  }
+  60% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    right: 1400px;
+    top: 600px;
+  }
+}
+
+@keyframes meteoroStyle3 {
+  0% {
+    opacity: 0;
+    right: 300px;
+    top: 300px;
+  }
+  30% {
+    opacity: 1;
+  }
+  60% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    right: 1000px;
+    top: 800px;
+  }
+}
+
+@keyframes meteoroStyle4 {
+  0% {
+    opacity: 0;
+    right: 700px;
+    top: 300px;
+  }
+  30% {
+    opacity: 1;
+  }
+  60% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    right: 1400px;
+    top: 800px;
+  }
+}
+
+@keyframes lua {
+  0% {
+    box-shadow: 0 0 160px 0px #fff, 0 0 30px -4px #fff,
+      0 0 8px 2px rgba(255, 255, 255, 0.26);
+  }
+  50% {
+    box-shadow: 0 0 80px 0px #fff, 0 0 30px -4px #fff,
+      0 0 8px 2px rgba(255, 255, 255, 0.26);
+  }
+  100% {
+    box-shadow: 0 0 160px 0px #fff, 0 0 30px -4px #fff,
+      0 0 8px 2px rgba(255, 255, 255, 0.26);
+  }
+}
+
+@keyframes rotate {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
 }
 </style>
