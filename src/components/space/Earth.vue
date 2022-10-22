615a039cc22f5f710838f683363cac69811cdb29
Jose <rhwkd5012@naver.com>
2022-10-22 20:16:27 +0900

    지구의 파일 위치 변경

---
+++ src/components/space/Earth.vue
@@ -0,0 +1,257 @@
+<template>
+  <div id="earth-view"></div>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+import * as THREE from "three";
+import { OrbitControls } from "three-orbitcontrols-ts";
+
+export default defineComponent({
+  name: "Earth",
+  setup() {
+    interface uniform {
+      type: string;
+      value: any;
+    }
+    // Scene, Camera, Renderer
+    let renderer = new THREE.WebGLRenderer();
+    renderer.setClearColor(0x000000, 0);
+    let scene = new THREE.Scene();
+    let aspect = window.innerWidth / window.innerHeight;
+    let camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 1500);
+    let cameraRotation = 0;
+    let cameraRotationSpeed = 0.001;
+    let cameraAutoRotation = true;
+    let orbitControls = new OrbitControls(camera);
+
+    // Lights
+    let spotLight = new THREE.SpotLight(0xffffff, 1, 0, 10, 2);
+
+    // Planet Proto
+    let planetProto = {
+      sphere: function (size: number) {
+        let sphere = new THREE.SphereGeometry(size, 32, 32);
+
+        return sphere;
+      },
+      material: function (options: any) {
+        let material = new THREE.MeshPhongMaterial() as any;
+        if (options) {
+          for (var property in options) {
+            material[property] = options[property];
+          }
+        }
+
+        return material;
+      },
+      glowMaterial: function (intensity: number, fade: number, color: string) {
+        // Custom glow shader from https://github.com/stemkoski/stemkoski.github.com/tree/master/Three.js
+        let glowMaterial = new THREE.ShaderMaterial({
+          uniforms: {
+            c: {
+              type: "f",
+              value: intensity,
+            } as uniform,
+            p: {
+              type: "f",
+              value: fade,
+            } as uniform,
+            glowColor: {
+              type: "c",
+              value: new THREE.Color(color),
+            } as uniform,
+            viewVector: {
+              type: "v3",
+              value: camera.position,
+            } as uniform,
+          },
+          vertexShader: `
+          uniform vec3 viewVector;
+          uniform float c;
+          uniform float p;
+          varying float intensity;
+          void main() {
+            vec3 vNormal = normalize( normalMatrix * normal );
+            vec3 vNormel = normalize( normalMatrix * viewVector );
+            intensity = pow( c - dot(vNormal, vNormel), p );
+            gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+          }`,
+          fragmentShader: `
+          uniform vec3 glowColor;
+          varying float intensity;
+          void main() 
+          {
+            vec3 glow = glowColor * intensity;
+            gl_FragColor = vec4( glow, 1.0 );
+          }`,
+          side: THREE.BackSide,
+          blending: THREE.AdditiveBlending,
+          transparent: true,
+        });
+
+        return glowMaterial;
+      },
+      texture: function (
+        material: { [x: string]: string; needsUpdate: any },
+        property: string | number,
+        uri: any
+      ) {
+        let textureLoader = new THREE.TextureLoader() as any;
+        textureLoader.crossOrigin = true;
+        textureLoader.load(uri, function (texture: string) {
+          material[property] = texture;
+          material.needsUpdate = true;
+        });
+      },
+    };
+
+    let createPlanet = function (options: any) {
+      // Create the planet's Surface
+      let surfaceGeometry = planetProto.sphere(options.surface.size);
+      let surfaceMaterial = planetProto.material(options.surface.material);
+      let surface = new THREE.Mesh(surfaceGeometry, surfaceMaterial);
+
+      // Create the planet's Atmosphere
+      let atmosphereGeometry = planetProto.sphere(
+        options.surface.size + options.atmosphere.size
+      );
+      let atmosphereMaterialDefaults = {
+        side: THREE.DoubleSide,
+        transparent: true,
+      };
+      let atmosphereMaterialOptions = Object.assign(
+        atmosphereMaterialDefaults,
+        options.atmosphere.material
+      );
+      let atmosphereMaterial = planetProto.material(atmosphereMaterialOptions);
+      let atmosphere = new THREE.Mesh(atmosphereGeometry, atmosphereMaterial);
+
+      // Create the planet's Atmospheric glow
+      let atmosphericGlowGeometry = planetProto.sphere(
+        options.surface.size +
+          options.atmosphere.size +
+          options.atmosphere.glow.size
+      );
+      let atmosphericGlowMaterial = planetProto.glowMaterial(
+        options.atmosphere.glow.intensity,
+        options.atmosphere.glow.fade,
+        options.atmosphere.glow.color
+      );
+      let atmosphericGlow = new THREE.Mesh(
+        atmosphericGlowGeometry,
+        atmosphericGlowMaterial
+      );
+
+      // Nest the planet's Surface and Atmosphere into a planet object
+      let planet = new THREE.Object3D();
+      surface.name = "surface";
+      atmosphere.name = "atmosphere";
+      atmosphericGlow.name = "atmosphericGlow";
+      planet.add(surface);
+      planet.add(atmosphere);
+      planet.add(atmosphericGlow);
+
+      // Load the Surface's textures
+      for (let textureProperty in options.surface.textures) {
+        planetProto.texture(
+          surfaceMaterial,
+          textureProperty,
+          options.surface.textures[textureProperty]
+        );
+      }
+
+      // Load the Atmosphere's texture
+      for (let textureProperty in options.atmosphere.textures) {
+        planetProto.texture(
+          atmosphereMaterial,
+          textureProperty,
+          options.atmosphere.textures[textureProperty]
+        );
+      }
+
+      return planet;
+    };
+
+    let earth: any | undefined = createPlanet({
+      surface: {
+        size: 0.3,
+        material: {
+          bumpScale: 0.05,
+          specular: new THREE.Color("grey"),
+          shininess: 10,
+        },
+        textures: {
+          map: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthmap1k.jpg",
+          bumpMap:
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthbump1k.jpg",
+          specularMap:
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthspec1k.jpg",
+        },
+      },
+      atmosphere: {
+        size: 0.003,
+        material: {
+          opacity: 0.8,
+        },
+        textures: {
+          map: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthcloudmap.jpg",
+          alphaMap:
+            "https://s3-us-west-2.amazonaws.com/s.cdpn.io/141228/earthcloudmaptrans.jpg",
+        },
+        glow: {
+          size: 0.02,
+          intensity: 0.7,
+          fade: 7,
+          color: 0x93cfef,
+        },
+      },
+    });
+
+    // Scene, Camera, Renderer Configuration
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    camera.position.set(1, 1, 1);
+    orbitControls.enabled = !cameraAutoRotation;
+
+    scene.add(camera);
+    scene.add(spotLight);
+    scene.add(earth);
+
+    // Light Configurations
+    spotLight.position.set(2, 0, 1);
+
+    // Mesh Configurations
+    earth.receiveShadow = true;
+    earth.castShadow = true;
+    earth.getObjectByName("surface").geometry.center();
+
+    // On window resize, adjust camera aspect ratio and renderer size
+    window.addEventListener("resize", function () {
+      camera.aspect = window.innerWidth / window.innerHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+    });
+
+    // Main render function
+    let render = function () {
+      earth.getObjectByName("surface").rotation.y += (1 / 32) * 0.01;
+      earth.getObjectByName("atmosphere").rotation.y += (1 / 16) * 0.01;
+      if (cameraAutoRotation) {
+        cameraRotation += cameraRotationSpeed;
+        camera.position.y = 0;
+        camera.position.x = 2 * Math.sin(cameraRotation);
+        camera.position.z = 2 * Math.cos(cameraRotation);
+        camera.lookAt(earth.position);
+      }
+      requestAnimationFrame(render);
+      renderer.render(scene, camera);
+    };
+    onMounted(() => {
+      render();
+      (document.getElementById("earth-view") as HTMLElement).appendChild(
+        renderer.domElement
+      );
+    });
+  },
+});
+</script>
