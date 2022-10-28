1fb69c1f03adb18cfff926ecb17dc9cbf84590a9
HoCG <rhwkd5012@naver.com>
2022-10-28 13:18:23 +0900

    vuetify 추가

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import { createPinia } from "pinia";
-import "./styles/index.scss";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
-const pinia = createPinia();
+loadFonts();
 
-createApp(App).use(router).use(pinia).mount("#app");
+createApp(App).use(router).use(vuetify).mount("#app");
